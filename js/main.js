4f021ea94178c426d06ae34c717c4cf693118848
pikimaru <pikajunglist@gmail.com>
2022-08-03 14:15:50 +0200

    Reupload

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ import {genKey} from './genKey.js'
 import * as listOfTasks from './listOfTasks.js'
 import * as deleteFunc from './deleteButton.js'
 import * as projectFunction from './projectFunction.js'
-// import {addTask} from './taskFunction.js'
+
 import * as openTaskWindow from './openNewTaskWindowFunction.js'
 import * as tasksFunction from './taskFunction.js'
 import * as localStorageFunc from './localStorageFunc.js'
+
+
 
 
 
